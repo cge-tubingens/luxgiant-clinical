a64d6c6d851e3d4cacb4b3ac27145af0f9a6ee88
Luis Giraldo Gonzalez <luisggon@protonmail.com>
2024-05-28 11:51:11 +0200

    test these two transformers

--- tests/testing.py
+++ tests/testing.py
@@ -7,7 +7,7 @@ from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
 
 from luxgiant_clinical.ClinicalReport import contingency_table
-from luxgiant_clinical.Transformations import AgeCategory, InitialMotorSymptoms
+from luxgiant_clinical.Transformations import AgeCategory, InitialMotorSymptoms, HandYOnOff
 from luxgiant_clinical.Helpers import recover_columns_names
 
 # DATA_MANAS= '/mnt/0A2AAC152AABFBB7/data/LuxGiantMatched/AGESEXMATCHED_GAPINDIA_DATA_23.01.2024.dta'
@@ -28,11 +28,15 @@ age_pipe2 = Pipeline([
 initmotor_pipe = Pipeline([
     ('initmotor', InitialMotorSymptoms(output_col='inmotnonmot').set_output(transform='pandas'))
 ])
+onoff_pipe = Pipeline([
+    ('hyonoff', HandYOnOff(output_col='hyonoff').set_output(transform='pandas'))
+])
 
 adv_trns = ColumnTransformer([
     ('age_pipe1', age_pipe1, ['age_at_onset']),
     ('age_pipe2', age_pipe2, ['age_at_onset']),
-    ('initmotor_pipe', initmotor_pipe, ['initial_symptom_s___1', 'initial_symptom_s___2'])
+    ('initmotor_pipe', initmotor_pipe, ['initial_symptom_s___1', 'initial_symptom_s___2']),
+    ('handyonoff', onoff_pipe, ['on_off', 'b_if_the_patient_is_receiv'])
 ],
 remainder='passthrough').set_output(transform='pandas')
 
