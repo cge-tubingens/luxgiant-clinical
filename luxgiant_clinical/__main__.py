58d349b225408c60efa310d7de679146471c89c7
Luis Giraldo Gonzalez <luisggon@protonmail.com>
2024-07-17 10:19:33 +0200

    minor upgrades

--- luxgiant_clinical/__main__.py
+++ luxgiant_clinical/__main__.py
@@ -13,8 +13,8 @@ from Helpers import arg_parser
 
 def final_cleaning(df_data:pd.DataFrame)->pd.DataFrame:
 
-    from Transformations import InitialMotorSymptoms, HandYOnOff, HandYstage, ExposurePesticide, ComputingAverages, ComputingRatio, Categorizer, RecodeGeoZone
-    from Transformations import CleanDatscan, CleanPramipexole, Identity, HandYcorrector
+    from Transformations import InitialMotorSymptoms, HandYOnOff, HandYstage, ExposurePesticide, ComputingAverages, Subtype, Categorizer, RecodeGeoZone
+    from Transformations import CleanDatscan, CleanPramipexole, Identity, HandYcorrector, AgeCategory
     from Helpers import recover_columns_names
 
     age1_labels = {0: "<50", 1:">=50"}
@@ -26,10 +26,10 @@ def final_cleaning(df_data:pd.DataFrame)->pd.DataFrame:
 
     adv_trns = ColumnTransformer([
         ('identity', Identity().set_output(transform='pandas'), ['age_at_onset', 'years_of_education', 'PD_duration']),
-        ('pd_dur', Categorizer(cutoffs=[5], labels=pdsl_labels, output_col='pdsl', include_right=False), ['PD_duration']),
+        ('pd_dur', Categorizer(cutoffs=[5], labels=pdsl_labels, output_col='pdsl', include_right=True), ['PD_duration']),
         ('age_pipe1', Categorizer(cutoffs=[50], labels=age1_labels, output_col='agecat_1', include_right=False), ['age_at_onset']),
         ('age_pipe2', Categorizer(cutoffs=[40], labels=age2_labels, output_col='agecat_2', include_right=False), ['age_at_onset']),
-        ('age_pipe3', Categorizer(cutoffs=[21, 50, 60], labels=age3_labels, output_col='agecatmd', include_right=False), ['age_at_onset']),
+        ('age_pipe3', AgeCategory(status_col='Status', age_col='age', onset_col='age_at_onset').set_output(transform='pandas'), ['Status','age_at_onset', 'age']),
         ('age_pipe4', Categorizer(cutoffs=[30, 40, 50, 60, 70, 80], labels=age4_labels, output_col='agecent', include_right=True), ['age_at_onset']),
         ('edu_cat', Categorizer(cutoffs=[12], labels=edu_labels, output_col='education2', include_right=True), ['years_of_education']),
         ('initmotor_pipe', InitialMotorSymptoms(output_col='inmotnonmot').set_output(transform='pandas'), ['initial_symptom_s___1', 'initial_symptom_s___2']),
@@ -37,7 +37,7 @@ def final_cleaning(df_data:pd.DataFrame)->pd.DataFrame:
         ('handystages', HandYstage(output_col='hystage').set_output(transform='pandas'), ['hoehn_and_yahr_staging']),
         ('expopest', ExposurePesticide(output_col='exppesticide').set_output(transform='pandas'),[ 'nature_of_work___1', 'nature_of_work___2', 'over_your_lifetime_have_yo', 'during_your_lifetime_did_y'] ),
         ('num', ComputingAverages(output_col='num').set_output(transform='pandas'), ['tremor_7', 'tremor_at_rest_head_upper', 'tremor_at_rest_head_upper_4', 'tremor_at_rest_head_upper_2','tremor_at_rest_head_upper_3', 'tremor_at_rest_head_upper_5', 'action_or_postural_tremor', 'action_or_postural_tremor_2']),
-        ('dem', ComputingAverages(output_col='dem').set_output(transform='pandas'), ['falling', 'freezing_when_walking', 'walking', 'gait', 'postural_stability_respons']),
+        ('dem', ComputingAverages(output_col='den').set_output(transform='pandas'), ['falling', 'freezing_when_walking', 'walking', 'gait', 'postural_stability_respons']),
         ('recodegeo', RecodeGeoZone(output_col='zonecat').set_output(transform='pandas'), ['zone_of_origin']),
         ('fix_datscan', CleanDatscan(datscan_col='datscan', med_col='f_dopa_pet').set_output(transform='pandas'), ['datscan', 'f_dopa_pet']),
         ('pramipexole', CleanPramipexole(pramipex_col='pramipexole', ropinerole_col='ropinerole'), ['pramipexole', 'ropinerole'])
@@ -46,16 +46,10 @@ def final_cleaning(df_data:pd.DataFrame)->pd.DataFrame:
     
     df_1 = adv_trns.fit_transform(df_data)
     df_1.columns = recover_columns_names(df_1.columns)
-    
-    ratio_labels = {1:"Tremor Dominant", 2:"Indeterminate", 3:"Postural instability and gait difficulty"}
-    
-    ratio_pipe = Pipeline([
-        ('ratio', ComputingRatio(output_col='ratio').set_output(transform='pandas')),
-        ('ratio_cat', Categorizer(cutoffs=[1, 1.5], labels=ratio_labels, output_col='subtype', include_right=True))
-    ])
+
     
     scnd_trns = ColumnTransformer([
-        ('ratio_pipe', ratio_pipe, ['num', 'dem']),
+        ('subtype', Subtype(output_col='subtype', num_col='num', den_col='den'), ['num', 'den']),
         ('handystage', HandYcorrector(ref_col='hyonoff', status_col='Status').set_output(transform='pandas'), ['hyonoff', 'Status', 'hoehn_and_yahr_staging'])
     ],
     remainder='passthrough').set_output(transform='pandas')
